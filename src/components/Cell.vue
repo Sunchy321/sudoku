624e894f82e400024e98148327ce6c7d936b48e1
Sunchy321 <sunchy321@qq.com>
2024-08-07 11:30:07 +0800

    feat: basic sudoku display

--- src/components/Cell.vue
+++ src/components/Cell.vue
@@ -1,10 +1,20 @@
 <template>
-    <div class="cell">
-
+    <div class="cell" :class="`type-${type}`">
+        <template v-if="type === 'candidate'">
+            <div
+                v-for="value in 9" :key="value"
+                class="candidate"
+                v-show="candidateAllows(value)"
+            >{{ value }}</div>
+        </template>
+        <template v-else>
+            <div class="value">{{ model.value }}</div>
+        </template>
     </div>
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue';
 
 export type CellModel = {
     type: 'candidate';
@@ -14,4 +24,49 @@ export type CellModel = {
     value: number;
 };
 
+const model = defineModel<CellModel>({ required: true });
+
+const type = computed(() => model.value?.type ?? 'candidate');
+
+const candidateAllows = (value: number) => {
+    if (model.value?.type !== 'candidate') {
+        return false;
+    }
+
+    return model.value.value.includes(value);
+};
+
 </script>
+
+<style lang="sass">
+
+.cell
+    display: grid
+    place-items: center
+    width: 100%
+    height: 100%
+
+    &.type-candidate
+        display: grid
+        grid-template-columns: repeat(3, 1fr)
+        grid-template-rows: repeat(3, 1fr)
+
+.value
+    top: 0
+    left: 0
+    width: 100%
+    height: 100%
+
+    text-align: center
+
+    font-size: 400%
+
+.candidate
+    font-size: 120%
+
+.value::before, .candidate::before
+        content: ''
+        display: inline-block
+        vertical-align: middle
+
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,13 +1,25 @@
 <template>
-    <q-page class="row items-center justify-evenly">
+    <q-page class="column items-center justify-evenly">
         <Sudoku v-model="sudoku" />
+
+        <q-input
+            class="q-mx-lg q-my-md"
+            outlined
+            v-model="code"
+            @keypress.enter="applyCode"
+        />
     </q-page>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
+
+import { useRoute, useRouter } from 'vue-router';
 
 import Sudoku, { SudokuModel } from 'src/components/Sudoku.vue';
+
+const router = useRouter();
+const route = useRoute();
 
 function defaultSudoku(): SudokuModel {
     const result = [];
@@ -20,5 +32,63 @@ function defaultSudoku(): SudokuModel {
 }
 
 const sudoku = ref(defaultSudoku());
+
+const code = ref('');
+
+const parseBoard = (board: string) => {
+    const digits = board.split(/(\[[^[\]]*\]|.)/).filter(x => x !== '').map(d => {
+        if (/^[0-9]$/.test(d)) {
+            return parseInt(d, 10);
+        }
+
+        if (/^\[[0-9]+\]$/.test(d)) {
+            return d.slice(1, -1).split('').map(Number).sort();
+        }
+
+        return null;
+    });
+
+    if (digits.length < 81) {
+        digits.push(...Array(81 - digits.length).fill(null));
+    }
+
+    if (digits.length > 81) {
+        digits.splice(81, digits.length - 81);
+    }
+
+    return digits;
+};
+
+const applyCode = () => {
+    const digits = parseBoard(code.value);
+
+    const board = digits.map(d => {
+        if (typeof d === 'number') {
+            return d.toString();
+        } else if (Array.isArray(d)) {
+            return `[${d.join('')}]`;
+        } else {
+            return '.';
+        }
+    });
+
+    router.replace({ query: { board } });
+};
+
+watch(() => route.query.board, () => {
+    const digits = parseBoard(route.query.board as string);
+
+    console.log(digits);
+
+    sudoku.value = digits.map(d => {
+        if (typeof d === 'number') {
+            return { type: 'value', value: d };
+        } else if (Array.isArray(d)) {
+            return { type: 'candidate', value: d };
+        } else {
+            return { type: 'candidate', value: [] };
+        }
+    });
+}, { immediate: true });
 
 </script>
